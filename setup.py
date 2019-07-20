9b0f57bb1685ead95cef03be82535aef321e22d9
engineer-heng <engineer_heng@outlook.com>
2019-07-20 20:51:31 +0800

    Update setup

--- setup.py
+++ setup.py
@@ -123,7 +123,7 @@ setup(
     #   py_modules=["my_module"],
     #
     # packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
-    packages=find_packages(),  # Required
+    packages=find_packages(exclude=['.vscode', 'doc']),  # Required
 
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
